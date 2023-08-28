30c32f48a5d0a6fa09e225990939d93deafbf417
Taylor McGregor <mcgregor.taylor44@gmail.com>
2023-08-28 14:33:39 -0500

    completed exercises 1 and 2

---
+++ OperatorExercise/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class Circle
+    {
+        public static double AreaOfCircle( double radius)
+        {
+         radius = (Math.PI * radius * radius);
+            return radius;
+        }
+    }
+}
--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x = 100; //Exercise 1
+            int y = 2;
+            Console.WriteLine(x + y);
+            Console.WriteLine(x - y);
+            Console.WriteLine(x * y);
+            Console.WriteLine(x / y);
+            Console.WriteLine(x % ++y);
+
+            Console.WriteLine("Hello! Please input a number " +
+                "to be used as the radius of a circle.");
+            double o = double.Parse(Console.ReadLine());
+            string formattedArea = o.ToString("0.00");
+            Console.WriteLine(Circle.AreaOfCircle(o));
         }
     }
 }
