aad5a3197c7ad57c3930aac9e0aa49b7675b37aa
Taylor McGregor <mcgregor.taylor44@gmail.com>
2023-08-28 21:31:16 -0500

    cleaned exercise two

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,19 +4,20 @@
     {
         static void Main(string[] args)
         {
-            int x = 100; //Exercise 1
-            int y = 2;
-            Console.WriteLine(x + y);
-            Console.WriteLine(x - y);
-            Console.WriteLine(x * y);
-            Console.WriteLine(x / y);
-            Console.WriteLine(x % ++y);
+            //    int x = 100; //Exercise 1
+            //    int y = 2;
+            //    Console.WriteLine(x + y);
+            //    Console.WriteLine(x - y);
+            //    Console.WriteLine(x * y);
+            //    Console.WriteLine(x / y);
+            //    Console.WriteLine(x % ++y);
 
             Console.WriteLine("Hello! Please input a number " +
                 "to be used as the radius of a circle.");
-            double o = double.Parse(Console.ReadLine());
-            string formattedArea = o.ToString("0.00");
-            Console.WriteLine(Circle.AreaOfCircle(o));
+            double o;
+            double.TryParse(Console.ReadLine(), out o);
+            string formattedArea = Circle.AreaOfCircle(o).ToString("0.00");
+            Console.WriteLine(formattedArea);
         }
     }
 }
